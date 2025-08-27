42f6424a9e11d269eb8dd037050e5eddbdaa9d73
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-27 16:25:37 -0300

    Create binaryTree.h

---
+++ binaryTree.h
@@ -0,0 +1,26 @@
+#ifndef binaryTree_H_INCLUDED
+#define binaryTree_H_INCLUDED
+
+#include <iostream>
+
+// Generic tree node
+template <typename T>
+struct TreeNode {
+    T data;
+    TreeNode<T> *right; // Points to right child node
+    TreeNode<T> *left; // Points to left child node
+};
+
+// Generic linked tree
+template <typename T>
+struct Tree {
+    TreeNode<T> *root;
+};
+
+// Boot generic binary tree
+template <typename T>
+void bootTree(Tree<T> &tree) {
+    tree.root = NULL;
+};
+
+#endif // binaryTree_H_INCLUDED
